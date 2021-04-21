2ecc420b96d1b1cc3001ea6131c6d8829855d88c
kylemcq13 <kylemcquillen@gmail.com>
2021-04-21 17:08:33 -0400

    editing beer_recs.csv for app visibility

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from sklearn.metrics.pairwise import linear_kernel
 
 app = flask.Flask(__name__, template_folder='html_templates')
 
-df = pd.read_csv('./beer_recs.csv', error_bad_lines=False)
+url = 'https://media.githubusercontent.com/media/kylemcq13/beer_recommender/master/model/beer_recs.csv'
+df = pd.read_csv(url, error_bad_lines=False)
 
 #using lemmatized_text, create the corpus
 corpus = df['adjectives/nouns']
